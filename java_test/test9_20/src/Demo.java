b0d5697572c74e38e4cc09a1da331b1e63f9cc74
HappyBeryl <897131608@qq.com>
2019-09-20 23:37:09 +0800

    提交代码

---
+++ java_test/test9_20/src/Demo.java
@@ -0,0 +1,19 @@
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+
+import static java.lang.System.out;//导入静态成员
+
+public class Demo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		List <String> list1 = new ArrayList<String>();
+		List <String> list2 = new LinkedList();
+		
+		
+		System.out.println();
+		out.println();
+	}
+
+}
