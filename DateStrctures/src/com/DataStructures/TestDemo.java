c5092243384243e761a0a8d2f3b6c048bb9e476d
HappyBeryl <897131608@qq.com>
2019-11-06 23:50:28 +0800

    提交测试代码

--- DateStrctures/src/com/DataStructures/TestDemo.java
+++ DateStrctures/src/com/DataStructures/TestDemo.java
@@ -2,23 +2,35 @@ package com.DataStructures;
 
 public class TestDemo {
     public static void main(String[] args) {
-        MySingleList1 mySingleList = new MySingleList1();
-        mySingleList.addLast(2);
+        MySingleList mySingleList = new MySingleList();
+        mySingleList.addLast(8);
         mySingleList.addLast(1);
         mySingleList.addLast(3);
         mySingleList.addLast(4);
         mySingleList.addLast(2);
         mySingleList.addIndex(2,111);
-        boolean bool = mySingleList.contains(111);
-        System.out.println(bool);
+//        boolean bool = mySingleList.contains(111);
+//        System.out.println(bool);
 //        ListNode node = mySingleList.middleNode();
 //        System.out.println(node.data);
-        mySingleList.disPlay();
+ //       mySingleList.disPlay();
 //        ListNode newHead = mySingleList.reverseList();
 //        mySingleList.disPlay2(newHead);
        // mySingleList.reverseList2();
-        ListNode node = mySingleList.findKthToTail(2);
-        System.out.println(node.data);
+//      ListNode node = mySingleList.findKthToTail(2);
+         mySingleList.display();
+//        ListNode node = mySingleList.findKthToTail1(66);
+//        if(node == null) {
+//            System.out.println("不存在");
+//        } else {
+//            System.out.println(node.data);
+//        }
+
+//        ListNode node = mySingleList.reverseList();
+//        mySingleList.disPlay2(node);
+        ListNode node1 = mySingleList.middleNode();
+        System.out.println(node1.data);
+
 
 
 
