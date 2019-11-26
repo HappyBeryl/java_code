c98b212c8ed5fbc61fd3bcc7011116ba83e93197
123 <bq51398@163.com>
2019-11-26 23:34:39 +0800

    提交测试代码

---
+++ java_test/Test/src/com/testthinkinginjava/Aclass.java
@@ -0,0 +1,13 @@
+package com.testthinkinginjava;
+
+public class Aclass {
+    private  String s;
+    Aclass() {
+        System.out.println("classA");
+    }
+}
+
+class Bclass {
+    String s = "sssss";
+    Bclass bclass = new Bclass();
+}
