a32fd91d1fe5a91a85241c4edd3e92318b9ce9bc
123 <897131608@qq.com>
2019-11-27 20:07:49 +0800

    提交测试代码

--- java_test/DoubleList/src/DoubleList.java
+++ java_test/DoubleList/src/DoubleList.java
@@ -169,3 +169,41 @@ public class DoubleList {
 
 
 }
+
+
+class Money implements Cloneable{
+  double money = 12.5;
+
+    @Override
+    protected Object clone() throws CloneNotSupportedException {
+        return super.clone();
+    }
+}
+
+class Person implements Cloneable {
+    public String name;
+    public Money m;
+    public Person() {
+        this.m  = new Money();
+    }
+
+
+    @Override
+    protected Object clone() throws CloneNotSupportedException {
+       // return super.clone();
+        Person per = (Person) super.clone();
+        per.m = (Money)this.m.clone();
+        return per;
+    }
+
+    public static void main(String[] args) throws CloneNotSupportedException {
+        Person person = new Person();
+        person.name = "caocao";
+        Person person1 = (Person) person.clone();
+        System.out.println(person.name);
+        System.out.println(person1.name);
+        person1.name = "hh";
+        System.out.println(person.name);
+        System.out.println(person1.name);
+    }
+}
