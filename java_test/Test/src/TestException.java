70bd9f5436dac2a272d73afceaf0b9d649abb170
123 <897131608@qq.com>
2019-11-30 22:01:27 +0800

    提交测试关于异常代码

---
+++ java_test/Test/src/TestException.java
@@ -0,0 +1,60 @@
+//泛型 容器=集合 18-22web
+
+class MyException extends Exception {
+    public MyException(String s) {
+        super(s);
+    }
+}
+
+
+public class TestException {
+    public static void main1(String[] args) {
+        int a = 10;
+        try {
+            if (a == 10) {
+                throw new Exception("异常");
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            System.out.println("finally");
+        }
+    }
+
+
+    //2、
+    public static void main2(String[] args) {
+        String str = null;
+        try{
+            System.out.println(str.length());
+        } catch (NullPointerException e) {
+            e.printStackTrace();
+            System.out.println("空指针异常");
+        }
+        System.out.println("java");
+    }
+
+    public static void main3(String[] args) {
+        int[] array = {1,2,3};
+        try {
+            array[4] = 99;
+        } catch (ArrayIndexOutOfBoundsException e) {
+            e.printStackTrace();
+            System.out.println("数组越界异常");
+        }
+        System.out.println("hello");
+    }
+
+    public static void main(String[] args)  {
+        int a = 10;
+        try {
+            if (a == 10) {
+                throw new   MyException("异常");
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
+
+
