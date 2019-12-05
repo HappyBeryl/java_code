7b776205b069624deedbb0cc17b7eb84fab0d1b7
123 <897131608@qq.com>
2019-12-05 23:12:23 +0800

    提交测试代码

--- java_test/TestDemo/src/TestException.java
+++ java_test/TestDemo/src/TestException.java
@@ -1,5 +1,7 @@
 //泛型 容器=集合 18-22web
 
+import java.util.Scanner;
+
 class MyException extends Exception {
     public MyException(String s) {
         super(s);
@@ -62,15 +64,56 @@ public class TestException {
         System.out.println(str2);
     }
 
-    public static void main(String[] args) {
+    public static void main26(String[] args) {
         StringBuilder sb = new StringBuilder("abc");
         System.out.println(sb);
         sb.append("java");
         System.out.println(sb);
         System.out.println(sb.append("java"));
+        //异常
+        System.out.println(10/0);
+        int[] arr = {1,2,3};
+        System.out.println(arr[100]);
+
     }
 
+    //测试异常
+    public static void main(String[] args) {
+        int[] arr = {1, 2, 3};
+        try {
+            System.out.println("before");
+            System.out.println(arr[100]);
+            System.out.println("afer");
+        } catch (ArrayIndexOutOfBoundsException e) {
+            e.printStackTrace();
+        }
+        System.out.println("afer try catch");
+        try(Scanner sc = new Scanner(System.in)) {
+            int num = sc.nextInt();
+            System.out.println("sum = " + num);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 
+    public static int divide(int x, int y) {
+        if (y == 0) {
+            throw new ArrayIndexOutOfBoundsException("抛出除0异常");
+        }
+        return x/y;
+    }
+
+    private static void login(String userName, String password) {
+        if (userName.equals(userName)) {
+            System.out.println("处理用户名错误");
+        }
+        if (password.equals(password)) {
+            System.out.println("处理密码错误");
+        }
+        System.out.println("登陆成功");
+    }
+
+    
 }
 
 
