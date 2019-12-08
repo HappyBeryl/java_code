a455856298f63fbf3b89c0d7fca5aa13535f8003
123 <897131608@qq.com>
2019-12-08 21:10:15 +0800

    提交测试代码

--- java_test/TestDemo/src/TestDemo.java
+++ java_test/TestDemo/src/TestDemo.java
@@ -1,5 +1,10 @@
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
+
 public class TestDemo {
-    public static void main(String[] args) {
+    public static void main1(String[] args) {
         char ch = '呵';
         System.out.println(ch);
 
@@ -43,6 +48,53 @@ public class TestDemo {
         }
         return f3;
     }
+
+    public static void main2(String[] args) {
+        Collection<String> collection = new ArrayList<>();
+        collection.add("Hello");
+        collection.add("Java");
+        System.out.println(collection.isEmpty());
+        System.out.println(collection.size());
+        //打印方式1
+        System.out.println(collection);
+        collection.remove("Java");
+        System.out.println(collection);
+        for(String s: collection) {
+            System.out.println(s);
+        }
+    }
+
+    public static void main3(String[] args) {
+        Map<String, String> map = new HashMap<>();
+        map.put("及时雨", "宋江");
+        map.put("黑旋风", "李逵");
+        System.out.println(map);
+        String str = map.getOrDefault("及时雨1", "武松");
+        System.out.println(str);
+        System.out.println(map.containsKey("黑旋风"));
+        System.out.println(map.isEmpty());
+        System.out.println(map.size());
+        for(Map.Entry<String, String> entry : map.entrySet()) {
+            System.out.println(entry.getKey() + " " + entry.getValue());
+        }
+    }
+
+    public static void main(String[] args) {
+        //int --> Integer 自动装箱
+        Integer a = 10;
+        //手动装箱
+        Integer b = Integer.valueOf(20);
+        System.out.println(b); //20
+        //Integer --> int 自动拆箱
+        int c = a;
+        //手动拆箱 还可以是double ...
+        int d = a.intValue();
+        System.out.println(b); //10
+
+    }
+
+
+
 
 
 }
