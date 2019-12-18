de1c722f8c8ba9f4a0fcb2cdd53562c27a205a40
123 <897131608@qq.com>
2019-12-18 21:13:49 +0800

    提交测试代码

--- DateStrcture/src/tree/com/BinaryTree.java
+++ DateStrcture/src/tree/com/BinaryTree.java
@@ -1,4 +1,7 @@
 package tree.com;
+
+import java.util.Stack;
+
 class TreeNode {
     char value;
     TreeNode left;
@@ -42,6 +45,17 @@ public class BinaryTree {
         preOrderTraversal(root.left);
         preOrderTraversal(root.right);
     }
+    //非递归前序遍历
+    void preOrderTraversalNor(TreeNode root) {
+        Stack<TreeNode> stack = new Stack<>();
+        TreeNode cur = root;
+        while (cur != null) {
+            System.out.println(cur.value);
+            stack.push(cur);
+        }
+        cur = stack.pop();
+        cur = cur.right;
+    }
 
     //中序遍历   //D B E H A C F G
     void inOrderTraversal(TreeNode root) {
@@ -59,5 +73,48 @@ public class BinaryTree {
         preOrderTraversal(root.right);
         System.out.print(root.value + " ");
     }
+
+    //10.二叉树的最大深度
+    public int maxDepth(TreeNode root) {
+        if(root == null) {
+            return 0;
+        }
+        int leftHight = maxDepth(root.left);
+        int rightHight = maxDepth(root.right);
+
+        return leftHight > rightHight
+                ? leftHight + 1 : rightHight + 1;
+    }
+    //11.判断一颗二叉树是否为平衡二叉树
+    public boolean isBalanced(TreeNode root) {
+        if(root == null) {
+            return true;
+        }
+        int leftHight = maxDepth(root.left);
+        int rightHight = maxDepth(root.right);
+
+        return Math.abs(leftHight - rightHight) <=1 &&
+                isBalanced(root.left) && isBalanced(root.right);
+    }
+
+    //12.镜像（对称）二叉树
+    public boolean isSymmetricChild(TreeNode leftTree, TreeNode rightTree) {
+        if(leftTree == null && rightTree!= null || leftTree != null && rightTree == null) {
+            return false;
+        }
+        if(leftTree == null && rightTree == null) {
+            return true;
+        }
+        return leftTree.value == rightTree.value &&
+                isSymmetricChild(leftTree.left,rightTree.right)
+                &&isSymmetricChild(leftTree.right,rightTree.left);
+    }
+    public boolean isSymmetric(TreeNode root) {
+        if (root == null) {
+            return false;
+        }
+        return isSymmetricChild(root.left , root.right);
+    }
+
 
 }
