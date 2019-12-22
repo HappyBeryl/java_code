5998d7f5f45bfce614c89bba5fbf2667670f71b1
123 <897131608@qq.com>
2019-12-22 23:28:08 +0800

    提交测试代码

--- java_test/TestDemo/src/TestHeapDemo.java
+++ java_test/TestDemo/src/TestHeapDemo.java
@@ -1,8 +1,17 @@
+import java.util.PriorityQueue;
+import java.util.Queue;
+
 public class TestHeapDemo {
     public static void main(String[] args) {
         TestHeap testHeap = new TestHeap();
         int[] array = new int[]{12,33,5,7,1,14};
         testHeap.creatHeap(array);
         testHeap.show();
+        System.out.println();
+        Queue<Integer> queue = new PriorityQueue<>();
+        queue.offer(1);
+        queue.offer(2);
+        System.out.println(queue.poll()); //1
+        System.out.println(queue.peek()); //2
     }
 }
