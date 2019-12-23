24c4ece42769655673be4fee56a003525e819fa1
123 <897131608@qq.com>
2019-12-23 23:51:24 +0800

    提交测试代码

--- java_test/TestDemo/src/TestDemo.java
+++ java_test/TestDemo/src/TestDemo.java
@@ -1,5 +1,13 @@
 public class TestDemo {
     public static void main(String[] args) {
-
+        TestHeap.show();
+        TestHeap.pushHeap(100);
+        TestHeap.show();
+        TestHeap.popHeap();
+        TestHeap.show();
+        System.out.println(TestHeap.getHeapTop());
+        System.out.println("================");
+        TestHeap.heapSort();
+        TestHeap.show();
     }
 }
