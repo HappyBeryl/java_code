38a149e409225b5a6e77f5150928d9013e7e756d
123 <897131608@qq.com>
2019-12-24 23:08:53 +0800

    提交测试代码

--- java_test/TestDemo/src/TestDemo.java
+++ java_test/TestDemo/src/TestDemo.java
@@ -1,13 +1,5 @@
 public class TestDemo {
     public static void main(String[] args) {
-        TestHeap.show();
-        TestHeap.pushHeap(100);
-        TestHeap.show();
-        TestHeap.popHeap();
-        TestHeap.show();
-        System.out.println(TestHeap.getHeapTop());
-        System.out.println("================");
-        TestHeap.heapSort();
-        TestHeap.show();
+
     }
 }
