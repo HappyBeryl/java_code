0b7c6d9d028b761be2c9f134cf923b215ef20035
123 <897131608@qq.com>
2019-12-26 20:35:11 +0800

    提交测试代码

--- java_test/TestDemo/src/TestSortDemo.java
+++ java_test/TestDemo/src/TestSortDemo.java
@@ -52,6 +52,7 @@ public class TestSortDemo {
      */
 
     /**
+     * 选择排序
      *时间复杂度：O(N^2) 一直是
      * 空间复杂度：O（1）
      * 稳定性：不稳定
@@ -69,6 +70,66 @@ public class TestSortDemo {
         }
     }
 
+    /**
+     * 冒泡排序
+     * 时间复杂度 O（N^2）  优化过为O（N） 数据有序
+     *空间复杂度O（1）
+     * 稳定的
+     */
+
+    /**
+     * 快速排序
+     *时间复杂度  O(Nlog2N)      K
+     *空间复杂度  O(log2N)
+     * 不稳定
+     */
+
+    public static int partion(int[] array, int start, int end) {
+        int tmp = array[start];
+        while (start < end) {
+            while ((start < end) && array[end] >= tmp) {
+                end--;
+            }
+            if (start >= end) {
+                array[start] = tmp;
+                break;
+            } else {
+                array[start] = array[end];
+            }
+
+            while ((start < end ) && array[start] <= tmp) {
+                start++;
+            }
+            if (start >= end) {
+                array[start] = tmp;
+                break;
+            } else {
+                array[end] = array[start];
+            }
+        }
+        return start;
+    }
+
+    //优化
+    public static int partion2(int[] array, int start, int end) {
+        int tmp = array[start];
+        while (start < end) {
+            while ((start < end) && array[end] >= tmp) {
+                end--;
+            }
+            array[start] = array[end];
+
+            while ((start < end ) && array[start] <= tmp) {
+                start++;
+            }
+                array[end] = array[start];
+        }
+        array[start] = tmp;
+        return start;
+    }
+
+
+
     public static void main(String[] args) {
         int[] array1 = new int[]{8,7,10,5,3,9};
 //        insertSort(array1);
@@ -76,36 +137,5 @@ public class TestSortDemo {
         System.out.println(Arrays.toString(array1));
     }
 
-    //"======================"
-    public static void insertSort2(int[] array) {
-        for (int i = 1; i < array.length; i++) {
-            int tmp = array[i];
-            int j = 0;
-            for (j = i-1; j >= 0 ; j--) {
-                if(array[j] > tmp) {
-                    array[j+1] = array[j];
-                }else {
-                    break;//前面的序列已经有序
-                }
-            }
-            //
-            array[j+1] = tmp;
-        }
-    }
 
-
-    public static void shell(int[] array,int gap) {
-        for (int i = gap; i < array.length; i++) {
-            int tmp = array[i];
-            int j = 0;
-            for (j = i-gap; j >= 0 ; j -= gap) {
-                if(array[j] > tmp) {
-                    array[j+gap] = array[j];
-                }else {
-                    break;
-                }
-            }
-            array[j+gap] = tmp;
-        }
-    }
 }
