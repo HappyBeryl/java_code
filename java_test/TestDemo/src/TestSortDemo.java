3f6def36d0d5aba3225d7fc3f1d142bbf3e6656e
123 <897131608@qq.com>
2019-12-28 23:38:46 +0800

    提交测试代码

--- java_test/TestDemo/src/TestSortDemo.java
+++ java_test/TestDemo/src/TestSortDemo.java
@@ -206,4 +206,160 @@ public class TestSortDemo {
 
 
 
+
+    /**
+     * 时间复杂度：O(n^2)
+     * 空间复杂度：O(1)
+     * 稳定性：稳定
+*/
+     public static int partion(int[] array,int start,int end) {
+     int tmp = array[start];
+     while (start < end) {
+     while ((start < end) && array[end] >= tmp) {
+     end--;
+     }
+     if(start >= end) {
+     break;
+     }else {
+     array[start] = array[end];
+     }
+     while ((start < end) && array[start] <= tmp) {
+     start++;
+     }
+     if(start >= end) {
+     break;
+     }else {
+     array[end] = array[start];
+     }
+     }
+     array[start] = tmp;
+     return start;
+     }
+
+     public static void insertSort2(int[] array,int low,int high) {
+     for (int i = low+1; i <= high; i++) {
+     int tmp = array[i];
+     int j = 0;
+     for (j = i-1; j >= low ; j--) {
+     if(array[j] > tmp) {
+     array[j+1] = array[j];
+     }else {
+     break;//前面的序列已经有序
+     }
+     }
+     //
+     array[j+1] = tmp;
+     }
+     }
+
+     public static void swap(int[] array,int low,int high) {
+     int tmp = array[low];
+     array[low] = array[high];
+     array[high] = tmp;
+     }
+
+
+     public static void ThreeNumOfMiddle
+     (int[] array,int low,int high) {
+     //array[mid] <= array[low] <= array[high];
+     int mid = (low+high)/2;
+     //array[mid] <= array[high]
+     if(array[mid] > array[high]) {
+     swap(array,mid,high);
+     }
+     //array[mid] <= array[low]
+     if(array[mid] > array[low]) {
+     swap(array,mid,low);
+     }
+     //array[low] <= array[high]
+     if(array[low] > array[high]) {
+     swap(array,low,high);
+     }
+     }
+
+     public static void quick(int[] array,int low,int high) {
+     //if(low == high) {}
+     //if(low > high) {}
+     if(low >= high) {
+     return;
+     }
+
+     if(high-low+1 < 100) {
+     insertSort2(array,low,high);
+     return;
+     }
+
+     ThreeNumOfMiddle(array,low,high);
+
+
+     int pivot = partion(array,low,high);
+
+     quick(array,low,pivot-1);
+     quick(array,pivot+1,high);
+     /*if(low < high) {
+     int pivot = partion(array,low,high);
+     quick(array,low,pivot-1);
+     quick(array,pivot+1,high);
+     }
+}
+
+    /**
+     * 时间复杂度：O(nlog2n)    最坏情况：O(n^2) 数据有序
+     * 空间复杂度：O(log2n)
+     * 稳定性：不稳定
+     * @param array
+     */
+    public static void quickSort(int[] array) {
+        quick2(array,0,array.length-1);
+    }
+
+    public static void quick2(int[] array,int low,int high) {
+        int pivot = partion(array,low,high);
+
+        Stack<Integer> stack = new Stack<>();
+        if(pivot > low+1 ) {
+            stack.push(low);
+            stack.push(pivot-1);
+        }
+        if(pivot < high-1) {
+            stack.push(pivot+1);
+            stack.push(high);
+        }
+        while (!stack.empty()) {
+            high = stack.pop();
+            low = stack.pop();
+            pivot = partion(array,low,high);
+            if(pivot > low+1 ) {
+                stack.push(low);
+                stack.push(pivot-1);
+            }
+            if(pivot < high-1) {
+                stack.push(pivot+1);
+                stack.push(high);
+            }
+        }
+
+    }
+    public static void main1(String[] args) {
+        int[] array = new int[10000000];
+        /*for (int i = 0; i < array.length; i++) {
+            array[i] = i;
+        }*/
+        Random random = new Random();
+        for (int i = 0; i < array.length; i++) {
+            array[i] = random.nextInt(100000);
+        }
+
+        //System.out.println(Arrays.toString(array));
+        long time1 = System.currentTimeMillis();
+        quickSort(array);
+        long time2 = System.currentTimeMillis();
+        System.out.println(time2-time1);
+        //System.out.println(Arrays.toString(array));
+    }
+
+
+
+
+
 }
