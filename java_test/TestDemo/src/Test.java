7dce51ff9178ddfaca3b4c96dd150d49a3d68bd4
123 <897131608@qq.com>
2020-01-09 14:00:31 +0800

    提交测试代码

--- java_test/TestDemo/src/Test.java
+++ java_test/TestDemo/src/Test.java
@@ -180,6 +180,34 @@ public class Test {
         return ret;
     }
 
+    //杨辉三角
+    //numRows：你的行数
+    public static List<List<Integer>> generate(int numRows) {
+
+        List<List<Integer>> triangle = new ArrayList<>();
+
+        if(numRows == 0) {
+            return triangle;
+        }
+        triangle.add(new ArrayList<>());//0
+        triangle.get(0).add(1);
+        //行数
+        for (int i = 1; i < numRows; i++) {
+            List<Integer> curRow = new ArrayList<>();
+            curRow.add(1);
+            //上一行
+            List<Integer> prevRow = triangle.get(i-1);
+            //第i行的第j列
+            for (int j = 1; j < i; j++) {
+                int tmp = prevRow.get(j-1)+prevRow.get(j);
+                curRow.add(tmp);
+            }
+            curRow.add(1);
+            triangle.add(curRow);
+        }
+        return triangle;
+    }
+
 
 
 
