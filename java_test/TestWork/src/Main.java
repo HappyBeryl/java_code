cc8d4c19bb3fae81bfbc2f76fae3a8d0bd39286b
123 <897131608@qq.com>
2020-01-10 23:11:10 +0800

    提交旋转数组代码

--- java_test/TestWork/src/Main.java
+++ java_test/TestWork/src/Main.java
@@ -1,3 +1,5 @@
+import java.util.Arrays;
+
 public class Main {
     /**
      * 大写转小写
@@ -6,10 +8,10 @@ public class Main {
      */
     public String toLowerCase(String str) {
         StringBuffer sb = new StringBuffer();
-        for(int i = 0; i < str.length(); i++) {
+        for (int i = 0; i < str.length(); i++) {
             char ch = str.charAt(i);
-            if(ch >= 65 && ch <= 90) {
-                ch = (char)(ch + 32);
+            if (ch >= 65 && ch <= 90) {
+                ch = (char) (ch + 32);
             }
             sb.append(ch);
         }
@@ -23,24 +25,41 @@ public class Main {
     /**
      * 给定一个数组，将数组中的元素向右移动 k 个位置，其中 k 是非负数
      * 给定一个数组，将数组中的元素向右移动 k 个位置，其中 k 是非负数。
-     *
-     * 示例 1:
-     * 输入: [1,2,3,4,5,6,7] 和 k = 3
-     * 输出: [5,6,7,1,2,3,4]
-     * 解释:
-     * 向右旋转 1 步: [7,1,2,3,4,5,6]
-     * 向右旋转 2 步: [6,7,1,2,3,4,5]
-     * 向右旋转 3 步: [5,6,7,1,2,3,4]
      */
     public void rotate(int[] nums, int k) {
         for (int i = 0; i < k; i++) {
-            int last = nums[nums.length-1];
-            for(int j = 0; j < nums.length; j++) {
+            int cur = nums[nums.length - 1];
+            for (int j = 0; j < nums.length; j++) {
                 int tmp = nums[j];
-                nums[j] = last;
-                last = tmp;
+                nums[j] = cur;
+                cur = tmp;
             }
         }
+    }
+
+    public static int[] rotate2(int[] nums, int k) {
+        reverse(nums, 0, nums.length - 1);
+        reverse(nums, 0, k - 1);
+        reverse(nums, k, nums.length - 1);
+        return nums;
+    }
+
+    public static void reverse(int[] nums, int first, int last) {
+        while (first <= last) {
+            int tmp = nums[first];
+            nums[first] = nums[last];
+            nums[last] = tmp;
+            first++;
+            last--;
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] nums = new int[]{1,2,3,4,5,6,7};
+        System.out.println(Arrays.toString(nums));
+        int[] array = rotate2(nums,3);
+        System.out.println(Arrays.toString(array));
+
     }
 
 }
