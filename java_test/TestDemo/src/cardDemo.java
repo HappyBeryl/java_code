a1b3e1f9861e1bd90eca8f3f561e43b61a17e64e
123 <897131608@qq.com>
2020-01-14 21:16:49 +0800

    提交测试List代码

--- java_test/TestDemo/src/cardDemo.java
+++ java_test/TestDemo/src/cardDemo.java
@@ -15,9 +15,8 @@ class Card {
     }
 }
 
-public class cardDemo {
-    public static final String[] suits = {"♥","♦",
-            "♣","♠"};
+public class CardDemo {
+    public static final String[] suits = {"♥","♦", "♣","♠"};
 
     public List<Card> buyPuke() {
         List<Card> deck = new ArrayList<>();
@@ -38,12 +37,39 @@ public class cardDemo {
         deck.set(end,tmp);
     }
 
+    //洗牌
     public void shuffle(List<Card> deck) {
         Random random = new Random(20191208);
         for (int i = deck.size()-1; i > 0 ; i--) {
             int index = random.nextInt(i);//[0,i)
             swap(deck,i,index);
         }
+    }
+
+    public static void main(String[] args) {
+        CardDemo cardDemo = new CardDemo();
+        List<Card> list = cardDemo.buyPuke();
+        System.out.println(list);
+        cardDemo.shuffle(list);
+        //发牌  3个人每个人最多发5张牌
+        System.out.println("发牌");
+        List<List<Card>> hands = new ArrayList<>();
+
+        List<Card> human1 = new ArrayList<>();
+        List<Card> human2 = new ArrayList<>();
+        List<Card> human3 = new ArrayList<>();
+
+        hands.add(human1);
+        hands.add(human2);
+        hands.add(human3);
+
+        for (int i = 0; i < 5; i++) {
+            for (int j = 0; j < 3; j++) {
+                hands.get(i).add(list.remove(0));
+            }
+        }
+        System.out.println("第一个人的牌");
+        System.out.println(human1);
     }
 
 }
