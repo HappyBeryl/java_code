0c027a88da8a52e4b607d07633758fbba7ec524f
123 <897131608@qq.com>
2020-01-18 21:51:50 +0800

    提交测试二叉树代码

--- DateStrcture/src/tree/com/BinaryTree.java
+++ DateStrcture/src/tree/com/BinaryTree.java
@@ -1,8 +1,6 @@
 package tree.com;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Stack;
+import java.util.*;
 
 class TreeNode {
     char value;
@@ -100,24 +98,12 @@ public class BinaryTree {
      }
 
 
-    public List<Character> inorderTraversal2(TreeNode root) {
-        Stack<TreeNode> stack = new Stack<>();
-        List<Character> list = new ArrayList<>();
-        TreeNode cur = root;
-        while(cur != null || !stack.empty()) {
-            while(cur != null) {
-                stack.push(cur);
-                cur = cur.left;
-            }
-            cur = stack.pop();
-            list.add(cur.value);
-            cur = cur.right;
-        }
-        return list;
-    }
+    /**
+     * 二叉树的后序遍历
+     * 递归与非递归
+     */
 
-
-    //后序遍历   A B D E H C F G
+    //左右根  A B D E H C F G
     void postOrderTraversal(TreeNode root) {
         inOrderTraversal(root.left);
         preOrderTraversal(root.right);
@@ -125,19 +111,184 @@ public class BinaryTree {
     }
 
     //非递归后序遍历
+    public List<Character> postorderTraversal1(TreeNode root) {
+        Stack<TreeNode> stack = new Stack<>();
+        List<Character> list = new ArrayList<>();
+        TreeNode cur = root;
+        TreeNode prev = null;
+        while (cur != null || !stack.empty()) {
+            while (cur != null) {
+                stack.push(cur);
+                cur = cur.left;
+            }
+            cur = stack.peek();
+            if (cur.right == null || cur.right == prev) {
+                stack.pop();
+                list.add(cur.value);
+                prev = cur;
+                cur = null;
+            } else {
+                cur = cur.right;
+            }
+        }
+        return list;
+    }
 
-    //10.二叉树的最大深度
+    /**
+     * 二叉树的层序遍历
+     */
+    public List<List<Character>> levelOrder(TreeNode root) {
+        Queue<TreeNode> queue = new LinkedList<>();
+        List<List<Character>> ret = new ArrayList<>();
+        if(root != null) {
+            queue.offer(root);
+        }
+        while(!queue.isEmpty()) {
+            int size = queue.size();
+            List<Character> list = new ArrayList<>();
+            while(size > 0) {
+                TreeNode cur = queue.poll();
+                list.add(cur.value);
+                size--;
+                if(cur.left != null) {
+                    queue.offer(cur.left);
+                }
+                if(cur.right != null) {
+                    queue.offer(cur.right);
+                }
+            }
+            ret.add(list);
+        }
+        return ret;
+    }
+
+    /**
+     * 求节点个数
+     * 子问题思路和遍历思路
+     */
+
+    int getSize(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+        return getSize(root.left) + getSize(root.right) + 1;
+    }
+
+
+    static int size = 0;
+    void getSize1(TreeNode root){
+        if(root == null) {
+            return ;
+        }
+        size++;
+        getSize1(root.left);
+        getSize1(root.right);
+    }
+
+    /**
+     * 求叶子节点个数
+     * 子问题思路和遍历思路
+     */
+    int getLeafSize(TreeNode root) {
+        if (root == null) {
+            return 0;
+        } else if(root.left == null && root.right == null) {
+            return 1;
+        }
+        return getLeafSize(root.left) + getLeafSize(root.right);
+    }
+
+    static int leafSize = 0;
+    void getLeafSize1(TreeNode root){
+        if(root == null) {
+            return ;
+        }else if(root.left == null && root.right == null) {
+            leafSize++;
+        }
+        getLeafSize1(root.left);
+        getLeafSize1(root.right);
+    }
+
+    /**
+     * 求第k层节点个数
+     * @param root
+     * @return
+     */
+
+    int getKLevelSize(TreeNode root,int k) {
+        if (root == null) {
+            return 0;
+        }
+        if (k == 1) {
+            return 1;
+        }
+        return getKLevelSize(root.left, k-1) + getKLevelSize(root.right, k-1);
+    }
+
+    /**
+     * 查找val所在节点
+     * @param root
+     * @return
+     */
+
+    TreeNode find(TreeNode root, int val) {
+        if (root == null) {
+            return null;
+        }
+        if (root.value == val) {
+            return root;
+        }
+
+        TreeNode ret = find(root.left, val);
+        if (ret != null) {
+            return ret;
+        }
+        ret = find(root.right, val);
+        if (ret != null) {
+            return ret;
+        }
+        return null; //没找到
+    }
+
+
+    /**
+     * 检查两棵树是否相同
+     */
+
+    public boolean isSameTree(TreeNode p, TreeNode q) {
+        if(p == null && q != null || p != null && q == null) {
+            return false;
+        }
+        if(p == null && q == null) {
+            return true;
+        }
+        if(p.value != q.value) {
+            return false;
+        }
+        return isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
+    }
+
+    /**
+     * 二叉树的最大深度
+     * @param root
+     * @return
+     */
+
     public int maxDepth(TreeNode root) {
         if(root == null) {
             return 0;
         }
-        int leftHight = maxDepth(root.left);
-        int rightHight = maxDepth(root.right);
-
-        return leftHight > rightHight
-                ? leftHight + 1 : rightHight + 1;
+        int lenLeft = maxDepth(root.left);
+        int lenRight = maxDepth(root.right);
+        return lenLeft > lenRight ? lenLeft+1 : lenRight+1;
     }
-    //11.判断一颗二叉树是否为平衡二叉树
+
+    /**
+     *判断一颗二叉树是否为平衡二叉树
+     * @param root
+     * @return
+     */
+
     public boolean isBalanced(TreeNode root) {
         if(root == null) {
             return true;
@@ -149,7 +300,9 @@ public class BinaryTree {
                 isBalanced(root.left) && isBalanced(root.right);
     }
 
-    //12.镜像（对称）二叉树
+    /**
+     *   镜像（对称）二叉树
+     */
     public boolean isSymmetricChild(TreeNode leftTree, TreeNode rightTree) {
         if(leftTree == null && rightTree!= null || leftTree != null && rightTree == null) {
             return false;
@@ -167,6 +320,38 @@ public class BinaryTree {
         }
         return isSymmetricChild(root.left , root.right);
     }
+
+    /**
+     * 判断一棵树是不是完全二叉树
+     */
+    boolean isCompleteTree(TreeNode root){
+        Queue<TreeNode> queue = new LinkedList<>();
+        if(root != null) {
+            queue.offer(root);
+        }
+        while (!queue.isEmpty()) {
+            TreeNode cur = queue.poll();
+            if(cur != null) {
+                queue.offer(cur.left);
+                queue.offer(cur.right);
+            }else {
+                break;
+            }
+        }
+
+        while (!queue.isEmpty()) {
+            TreeNode cur = queue.peek();
+            if(cur != null) {
+                return false;
+            }else {
+                queue.poll();
+            }
+        }
+        return true;
+    }
+
+    
+
 
 
 }
