6327736b9d6454a47caaa28f25980a54b4559a2f
123 <897131608@qq.com>
2020-01-21 21:45:36 +0800

    提交测试快速排序代码

--- java_test/TestDemo/src/TestSort.java
+++ java_test/TestDemo/src/TestSort.java
@@ -96,9 +96,58 @@ public class TestSort {
         }
     }
 
-    public static void main(String[] args) {
+    public static void main2(String[] args) {
         int[] array = new int[]{12, 5, 9, 34, 6, 8, 33, 56, 89, 0, 7, 4, 22, 55, 77};
         shellSort(array);
+        System.out.println(Arrays.toString(array));
+    }
+
+    public static int partion(int[] array, int start, int end) {
+        int tmp = array[start];
+        while (start < end) {
+            while ((start < end) && array[end] >= tmp) {  //9 3 2 9 10
+                end--;
+            }
+            if (start >= end) {
+                array[start] = tmp;
+                break;
+            } else {
+                array[start] = array[end];
+            }
+
+            while ((start < end) && array[start] <= tmp) {  //9 3 2 9 10
+                start++;
+            }
+            if (start >= end) {
+                array[start] = tmp; //
+                break;
+            } else {
+                array[end] = array[start];
+            }
+
+        }
+        return start;
+    }
+
+    public static void quick(int[] array, int low, int high) {
+        //递归的终止条件:只有一个元素
+        if (low >= high) { //=zuo  >you
+            return;
+        }
+        //1.写一个函数把待排序序列分为两部分
+        int pivot = partion(array, low, high); //low和high是局部变量
+        //开始递归 左右
+        quick(array, low, pivot-1);
+        quick(array, pivot+1, high);
+    }
+
+    public static void quickSort(int[] array) {
+        quick(array, 0, array.length-1);
+    }
+
+    public static void main(String[] args) {
+        int[] array = new int[]{27, 15, 19, 18, 28, 34, 65, 49, 25, 37};
+        quickSort(array);
         System.out.println(Arrays.toString(array));
     }
 }
