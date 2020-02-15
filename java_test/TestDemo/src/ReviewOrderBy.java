3253de78b7e4b04d9cf53d2c83053433eb4e1d9d
123 <897131608@qq.com>
2020-02-15 23:54:55 +0800

    复习排序

--- java_test/TestDemo/src/ReviewOrderBy.java
+++ java_test/TestDemo/src/ReviewOrderBy.java
@@ -75,10 +75,22 @@ public class ReviewOrderBy {
         }
     }
 
+    public static void selectSort(int[] array) {
+        for (int i = 0; i < array.length; i++) {
+            for (int j = i+1; j < array.length; j++) {
+                if (array[i] > array[j]) {
+                    int tmp =array[i];
+                    array[i] = array[j];
+                    array[j] = tmp;
+                }
+            }
+        }
+    }
 
+    
         public static void main(String[] args) {
         int[] array = new int[] {27, 15, 19, 18, 28, 34, 65, 49, 25, 37};
-        insertSort1(array);
+        selectSort(array);
         System.out.println(Arrays.toString(array));
     }
 
