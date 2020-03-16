3610ea974347109b75a0cf10662778465c57dab3
123 <897131608@qq.com>
2020-03-16 23:35:18 +0800

    提交测试代码

--- multithreading/TestDemo/src/com/thread1/case5.java
+++ multithreading/TestDemo/src/com/thread1/case5.java
@@ -2,16 +2,32 @@ package com.thread1;
 
 import com.sun.scenario.effect.impl.sw.sse.SSEBlend_SRC_OUTPeer;
 
+import java.util.Scanner;
+
 public class case5 {
     private static void A () {
         System.out.println("a");
     }
 
-    public static void main(String[] args) {
+    public static void main1(String[] args) {
         String s = "fmn";
         s.toUpperCase();
         String y = s.replace('f','F');
         y = y+"wxy";
         System.out.println(y);
     }
+
+    public static void main(String[] args) {
+        Thread thread = new Thread() {
+            @Override
+            public void run() {
+                dianping();
+            }
+        };
+        System.out.println("dazhong");
+    }
+    static void dianping() {
+        System.out.println("dianping");
+    }
+
 }
