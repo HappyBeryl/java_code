854a8ab04b788f006373ae332d1f603a2f0e25cc
123 <897131608@qq.com>
2020-03-19 17:43:33 +0800

    懒汉模式线程安全版本

--- DesignMode/src/com/singleton/Singleton2.java
+++ DesignMode/src/com/singleton/Singleton2.java
@@ -9,10 +9,15 @@ public class Singleton2 {
     private Singleton2() {}
 
     public static Singleton2 getInstance() {
-        if (instance == null) {
-            instance = new Singleton2();
+            if (instance == null) {
+                synchronized (Singleton2.class) {
+                    if (instance == null) {
+                        instance = new Singleton2();
+                    }
+            }
         }
         return instance;
+
     }
 
     static class MyThread extends Thread {
