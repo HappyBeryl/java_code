becd516ff86ee94e645fee9ccdee1c5e6cc49ff8
123 <897131608@qq.com>
2020-03-19 20:44:35 +0800

    归并非递归

--- java_test/TestDemo2/src/com/sort/TestDemo.java
+++ java_test/TestDemo2/src/com/sort/TestDemo.java
@@ -5,47 +5,100 @@ import java.util.Arrays;
 public class TestDemo {
 
     public static void merge(int[] array, int low, int mid, int high) {
-        int s1 = low;
-        int s2 = mid + 1;
-        int[] arrayTmp = new int[high - low + 1];
-        int i = 0;
-        while (s1 <= mid && s2 <= high) {
-            if (array[s1] <= array[s2]) {
-                arrayTmp[i++] = array[s1++];
+        int i = low;
+        int j = mid + 1;
+        int[] tmpArray = new int[high - low + 1];
+        int n = 0;
+        while (i <= mid && j <= high) {
+            if (array[i] <= array[j]) {
+                tmpArray[n++] = array[i++];
             } else {
-                arrayTmp[i++] = array[s2++];
+                tmpArray[n++] = array[j++];
             }
         }
-        while (s1 <= mid) {
-            arrayTmp[i++] = array[s1++];
+
+        while (i <= mid) {
+            tmpArray[n++] = array[i++];
         }
 
-        while (s2 <= high) {
-            arrayTmp[i++] = array[s2++];
+        while (j <= high) {
+            tmpArray[n++] = array[j++];
         }
-        for (int j = 0; j < arrayTmp.length; j++) {
-            array[low+j] = arrayTmp[j];
+
+        for (int k = 0; k < tmpArray.length; k++) {
+            array[low + k] = tmpArray[k];
         }
     }
 
     public static void mergeSortIntenal(int[] array, int low, int high) {
+        //递出去
         if (low >= high) {
             return;
         }
+
         int mid = (low + high) / 2;
         mergeSortIntenal(array, low, mid);
-        mergeSortIntenal(array, mid+1, high);
-        //合并
+        mergeSortIntenal(array, mid + 1, high);
         merge(array, low, mid, high);
     }
 
     public static void mergeSort(int[] array) {
-        mergeSortIntenal(array, 0, array.length-1);
+        mergeSortIntenal(array, 0 , array.length-1);
     }
 
-    public static void main25(String[] args) {
+
+    public static void mergeSort1(int[] array) {
+
+        //进行分组归并
+        for (int i = 1; i < array.length; i *= 2) {
+            merge(array,i);
+        }
+    }
+    //gap代表每个归并段的数据
+    public static void merge(int[] array,int gap) {
+        int[] tmpArray = new int[array.length];
+        int k = 0;
+
+        int s1 = 0;
+        int e1 = s1 + gap -1;
+        int s2 = e1 + 1;
+        int e2 = s2 + gap -1 < array.length ? s2 + gap -1 : array.length-1;
+
+        while (s2 < array.length) {
+            while (s1 <= e1 && s2 <= e2) {
+                if (array[s1] < array[s2]) {
+                    tmpArray[k++] = array[s1++];
+                } else {
+                    tmpArray[k++] = array[s2++];
+                }
+            }
+
+            while (s1 <= e1) {
+                tmpArray[k++] = array[s1++];
+            }
+
+            while (s2 <= e2) {
+                tmpArray[k++] = array[s2++];
+            }
+
+            s1 = e2 + 1;
+            e1 = s1 + gap - 1;
+            s2 = e1 + 1;
+            e2 = s2 + gap -1 < array.length ? s2 + gap -1 : array.length-1;
+        }
+
+        while (s1 < array.length) {
+            tmpArray[k++] = array[s1++];
+        }
+
+        for (int i = 0; i < tmpArray.length; i++) {
+            array[i] = tmpArray[i];
+        }
+    }
+
+    public static void main(String[] args) {
         int[] array = new int[]{10, 6, 7, 1, 3, 9, 4, 2};
-        mergeSort(array);
+        mergeSort1(array);
         System.out.println(Arrays.toString(array));
     }
 
