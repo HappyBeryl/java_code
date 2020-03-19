f4851ea7c39d6ee32b9e852786ff2b5cc53aaec8
123 <897131608@qq.com>
2020-03-19 11:34:12 +0800

    单例模式之饿汉模式

---
+++ DesignMode/src/com/singleton/Singleton1.java
@@ -0,0 +1,45 @@
+package com.singleton;
+
+import com.sun.scenario.effect.impl.sw.sse.SSEBlend_SRC_OUTPeer;
+
+/*
+ 饿汉模式
+ */
+public class Singleton1 {
+    //一开始就初始化
+    private static Singleton1 instance = new Singleton1();
+
+    private Singleton1() {}
+
+    public static Singleton1 getInstance() {
+        return instance;
+    }
+
+    static class MyThraed extends Thread {
+        @Override
+        public void run() {
+            Singleton1 ins1 = Singleton1.getInstance();
+            System.out.println(ins1);
+        }
+    }
+
+    public static void main(String[] args) {
+     /*   Singleton1 ins1 = Singleton1.getInstance();
+        Singleton1 ins2 = Singleton1.getInstance();
+        Singleton1 ins3 = Singleton1.getInstance();
+        System.out.println(ins1 == ins2);
+        System.out.println(ins2 == ins3);
+        运行结果为true
+     */
+
+     MyThraed[] threads = new MyThraed[20];
+        for (int i = 0; i < 20; i++) {
+            threads[i] = new MyThraed();
+        }
+
+        for (int i = 0; i < 20; i++) {
+            threads[i].start();
+        }
+    }
+
+}
