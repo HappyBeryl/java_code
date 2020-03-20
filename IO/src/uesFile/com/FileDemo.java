04a31d1d6aa3fb3bd9bdd27c5beb74aab48f7431
123 <897131608@qq.com>
2020-03-20 22:05:31 +0800

    提交测试创建文件相关代码

---
+++ IO/src/uesFile/com/FileDemo.java
@@ -0,0 +1,29 @@
+package uesFile.com;
+
+import java.io.File;
+import java.io.IOException;
+
+public class FileDemo {
+    public static void main(String[] args) throws IOException {
+
+        /*{
+            //普通文件的创建
+            String parent = "D:\\文档";
+            String fileName = "演示创建文件的文件.txt";
+
+            File file = new File(parent, fileName);
+            System.out.println(file.exists());
+            boolean success = file.createNewFile();
+            if (success) {
+                System.out.println(fileName + "被创建成功");
+            } else {
+                System.out.println(fileName + "已经存在");
+            }
+        }*/
+
+        {
+            //演示创建文件夹 midir 和 midirs
+
+        }
+    }
+}
