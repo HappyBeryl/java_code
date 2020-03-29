bbe0c847b0f1aa7f4816ce1b9d60a0fece462d65
123 <897131608@qq.com>
2020-03-29 22:50:28 +0800

    测试udp特点代码

--- Internet/src/udp/com/Client.java
+++ Internet/src/udp/com/Client.java
@@ -10,20 +10,17 @@ public class Client {
     private static final String serverIP = "127.0.0.1";
 
     public static void main(String[] args) throws IOException {
-        // 创建 UDP Socket 的
-        // 不需要传入端口，OS 自动分配
+
         try (DatagramSocket clientSocket = new DatagramSocket()) {
 
-            //  准备好请求，同时，传输的必须是字符格式
+            // 1准备好请求，同时，传输的必须是字符格式
             String request = "你好";
             byte[] requestBytes = request.getBytes(Server.CHARSET);
 
             // 发送请求
-            //  先准备 DatagramPacket
-            //     需要指定服务器的 ip + port
             DatagramPacket packetToServer = new DatagramPacket(
-                    requestBytes, 0, requestBytes.length,
-                    InetAddress.getByName(serverIP), Server.PORT    
+                    requestBytes, 0, requestBytes.length,     // 要发送的数据
+                    InetAddress.getByName(serverIP), Server.PORT    // 要发送到互联网的哪个进程上
             );
 
             clientSocket.send(packetToServer);
