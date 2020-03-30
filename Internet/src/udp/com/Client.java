4af954d9f640cb573971fb84149f28da190d6787
123 <897131608@qq.com>
2020-03-30 15:28:44 +0800

    提交 socket udp编程测试代码

--- Internet/src/udp/com/Client.java
+++ Internet/src/udp/com/Client.java
@@ -6,24 +6,19 @@ import java.net.DatagramSocket;
 import java.net.InetAddress;
 
 public class Client {
-    // 这里使用 127.0.0.1 代表本机
     private static final String serverIP = "127.0.0.1";
-
     public static void main(String[] args) throws IOException {
-
-        try (DatagramSocket clientSocket = new DatagramSocket()) {
-
-            // 1准备好请求，同时，传输的必须是字符格式
-            String request = "你好";
-            byte[] requestBytes = request.getBytes(Server.CHARSET);
-
+       try(DatagramSocket clientSocket = new DatagramSocket()){
+            String request = "hello";
+            //必须以字符格式进行传输
+            byte[] requesBytes = request.getBytes(Server.CHARSET);
             // 发送请求
-            DatagramPacket packetToServer = new DatagramPacket(
-                    requestBytes, 0, requestBytes.length,     // 要发送的数据
-                    InetAddress.getByName(serverIP), Server.PORT    // 要发送到互联网的哪个进程上
+            //  先准备 DatagramPacket 需要指定服务器的 ip + port
+            DatagramPacket packetToServe = new DatagramPacket(
+                    requesBytes, 0, requesBytes.length, //要发送的数据
+                    InetAddress.getByName(serverIP), Server.PORT //要发送到互联网的哪个进程
             );
-
-            clientSocket.send(packetToServer);
+          clientSocket.send(packetToServe);
         }
     }
 }
