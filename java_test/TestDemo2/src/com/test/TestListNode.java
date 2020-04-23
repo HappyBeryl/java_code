1ca51db839bf85cbc90ece6e3deed0e1cfbc8793
123 <897131608@qq.com>
2020-04-23 23:09:24 +0800

    复习单链表

--- java_test/TestDemo2/src/com/test/TestListNode.java
+++ java_test/TestDemo2/src/com/test/TestListNode.java
@@ -81,6 +81,45 @@ public class TestListNode {
         }
         return bs;
     }
+    public static ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+        if(headA == null || headB == null) {
+            return null;
+        }
+        ListNode pL = headA;//永远指向长的单链表
+        ListNode pS = headB;//永远指向短的单链表
+        int lenA = 0;
+        int lenB = 0;
+        //求的lenA  lenB
+        while (pL != null) {
+            lenA++;
+            pL = pL.next;
+        }
+        while (pS != null) {
+            lenB++;
+            pS = pS.next;
+        }
+        //pl、ps为空了
+        pL = headA;
+        pS = headB;
+        //差值-》最长的单链表先走len步
+        int len = lenA-lenB;
+        if(len < 0) {
+            pL = headB;
+            pS = headA;
+            len = lenB-lenA;
+        }
+        //让pL先走len步
+        for (int i = 0; i < len; i++) {
+            pL = pL.next;
+        }
+        //开始一起走  (pL  != pS ) {一人一步走}
+        while (pL != pS) {
+            pL = pL.next;
+            pS = pS.next;
+        }
+        return pL;
+    }
+
 
 
 
