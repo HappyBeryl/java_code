873d9daf8ce1ece007d30845d187309fcad50915
123 <897131608@qq.com>
2020-05-01 14:46:31 +0800

    阻塞队列练习

---
+++ java_test/TestDemo2/src/com/test/MyBlockingArrayQueue.java
@@ -0,0 +1,33 @@
+package com.test;
+
+public class MyBlockingArrayQueue {
+    int[] array = new int[1];
+    int front = 0;
+    int rear = 0;
+    int size = 0;
+
+    synchronized void put(int value) throws InterruptedException {
+        while (size == array.length) {
+            wait();
+        }
+        array[rear] = value;
+        rear++;
+        rear = (rear+1) % array.length;
+        size++;
+        notifyAll();
+    }
+
+    synchronized int take() throws InterruptedException {
+        while (size == 0) {
+            wait();
+        }
+
+        int value = array[front];
+        front++;
+        front = (front+1) % array.length;
+        size--;
+        notifyAll();
+
+        return value;
+    }
+}
