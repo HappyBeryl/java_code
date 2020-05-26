22205751dce398b7636a7499f6f37d70c0e671fe
123 <897131608@qq.com>
2020-05-26 23:08:14 +0800

    提交测试代码

--- java_test/TestDemo2/src/com/test/Main.java
+++ java_test/TestDemo2/src/com/test/Main.java
@@ -1,78 +1,56 @@
 package com.test;
 
-import java.util.*;
+class A {
+    static {
+        System.out.println("A.static {}");
+    }
 
-public class Main {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        while (sc.hasNext()) {
-            int n = sc.nextInt();
-            int option = sc.nextInt();
-            Student[] student = new Student[n];
-            for (int i = 0; i < n; i++) {
-                student[i] = new Student(sc.next(), sc.nextInt());
-            }
-            if (option == 1) {
-                //升序
-                ComparaUp cp = new ComparaUp();
-                Arrays.sort(student, cp);
+    static int i = initI();
 
-            } else if (option == 0) {
-                //降序
-                ComparaDown cd = new ComparaDown();
-                Arrays.sort(student, cd);
-            }
-            for (int i = 0; i < student.length; i++) {
-                System.out.println(student[i]);
-            }
-        }
+    static int initI() {
+        System.out.println("A.initI()");
+        return 0;
+    }
+
+    A() {
+        System.out.println("A()");
+    }
+
+    {
+        System.out.println("A.{}");
     }
 }
 
-class Student {
-    String name;
-    int score;
-
-    public Student(String name, int score) {
-        this.name = name;
-        this.score = score;
+class B extends A {
+    static {
+        System.out.println("B.static {}");
     }
 
-    @Override
-    public String toString() {
-        String ret = this.name + " " + this.score;
-        return ret;
-    }
-}
+    static int i = initI();
 
-class ComparaUp implements Comparator<Student> {
-    @Override
-    public int compare(Student o1, Student o2) {
-        if (o1 == o2) {
-            return 0;
-        }
-        if (o1 == null) {
-            return -1;
-        }
-        if (o2 == null) {
-            return 1;
-        }
-        return o1.score - o2.score;
+    static int initI() {
+        System.out.println("B.initI()");
+        return 0;
+    }
+
+    B() {
+        System.out.println("B()");
+    }
+
+    {
+        System.out.println("B.{}");
     }
 }
 
-class ComparaDown implements Comparator<Student> {
-    @Override
-    public int compare(Student o1, Student o2) {
-        if (o1 == o2) {
-            return 0;
+class Main extends B {
+    public static void main(String[] args) throws Exception {
+        System.out.println("start");
+        new B();
+        new B();
+        System.out.println("end");
+
+        while (true) {
+            Thread.sleep(1000);
         }
-        if (o1 == null) {
-            return -1;
-        }
-        if (o2 == null) {
-            return 1;
-        }
-        return o2.score - o1.score;
     }
 }
