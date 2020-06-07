3dfd61acea0de9454c5b972df0c3846820a5a1a6
123 <897131608@qq.com>
2020-06-07 22:36:54 +0800

    学习spring测试代码

--- java_test/TestDemo2/src/com/sort/TestDemo.java
+++ java_test/TestDemo2/src/com/sort/TestDemo.java
@@ -1,214 +1,43 @@
-package com.sort;
-
-import java.util.*;
-
-public class TestDemo {
-
-    /*
-    public static void quickSort(int[] array) {
-        quick(array, 0, array.length-1);
-    }
-
-    private static void quick(int[] array, int low, int high) {
-        if (low >= high) {
-            return;
-        }
-        int pivot = partion(array, low, high);
-        quick(array, low, pivot-1);
-        quick(array, pivot+1, high);
-    }
-
-    private static int partion(int[] array, int start, int end) {
-        int tmp = array[start];
-        while (start < end) {
-            while ((start < end) && array[end] >= tmp) {
-                end--;
-            }
-            if (start >= end) {
-                break;
-            } else {
-                array[start] = array[end];
-            }
-
-            while ((start < end) && array[start] <= tmp) {
-                start++;
-            }
-            if (start >= end) {
-                break;
-            } else {
-                array[end] = array[start];
-            }
-        }
-        array[start] = tmp;
-        return start;
-    }
-*/
-
-    public static void quickSort(int[] array) {
-        quick(array, 0, array.length-1);
-    }
-
-    private static void quick(int[] array, int low, int high) {
-        int pivot = partion(array, low, high);
-        Stack<Integer> stack = new Stack<>();
-        if (pivot > low + 1) {
-            stack.push(low);
-            stack.push(pivot-1);
-        }
-        if (pivot < high - 1) {
-            stack.push(pivot+1);
-            stack.push(high);
-        }
-        while (!stack.empty()) {
-            high = stack.pop();
-            low = stack.pop();
-            pivot = partion(array, low, high);
-            if (pivot > low + 1) {
-                stack.push(low);
-                stack.push(pivot-1);
-            }
-            if (pivot < high - 1) {
-                stack.push(pivot+1);
-                stack.push(high);
-            }
-        }
-    }
-
-    private static int partion(int[] array, int start, int end) {
-        int tmp = array[start];
-        while (start < end) {
-            while ((start < end) && array[end] >tmp) {
-                end--;
-            }
-            if (start >= end) {
-                break;
-            } else {
-                array[start] = array[end];
-            }
-
-            while ((start < end) && array[start] < tmp) {
-                start++;
-            }
-            if (start >= end) {
-                break;
-            } else {
-                array[end] = array[start];
-            }
-        }
-        array[start] = tmp;
-        return start;
-    }
-
-    public static void heapSort(int[] array) {
-        creatHeap(array);
-        int end = array.length-1;
-        while (end > 0) {
-            int tmp = array[0];
-            array[0] = array[end];
-            array[end] = tmp;
-            adjustDown(array, 0, end);
-            end--;
-        }
-    }
-
-    private static void creatHeap(int[] array) {
-        for (int i = (array.length-2)/2; i >= 0; i--) {
-            adjustDown(array, i, array.length);
-        }
-    }
-
-    private static void adjustDown(int[] array, int root, int len) {
-        int child = 2 * root + 1;
-        while (child < len) {
-            if (child + 1 < len && array[child] < array[child+1]) {
-                child++;
-            }
-            if (array[child] > array[root]) {
-                int tmp = array[child];
-                array[child] = array[root];
-                array[root] = tmp;
-                root = child;
-                child = 2 * root + 1;
-            } else {
-                break;
-            }
-        }
-    }
-
-    public static void bubbleSort(int[] array) {
-        boolean flg = false;
-        for (int i = 0; i < array.length-1; i++) { //趟数
-            for (int j = 0; j < array.length - 1 - i; j++) {
-                if (array[j] > array[j+1]) {
-                    int tmp = array[j];
-                    array[j] = array[j+1];
-                    array[j+1] = tmp;
-                    flg = true;
-                }
-            }if (!flg) {
-                break;
-            }
-
-        }
-    }
-
-    public static void selectSort(int[] array) {
-        for (int i = 0; i < array.length; i++) {
-            for (int j = i+1; j < array.length; j++) {
-                if (array[i] > array[j]) {
-                    int tmp = array[i];
-                    array[i] = array[j];
-                    array[j] = tmp;
-                }
-            }
-        }
-    }
-
-    public static void insertSort(int[] array) {
-        int j = 0;
-        for (int i = 1; i < array.length; i++) {
-            int tmp = array[i];
-            for (j = i-1; j >= 0; j--) {
-                if (array[j] > tmp) {
-                    array[j+1] = array[j];
-                } else {
-                    break;
-                }
-            }
-            array[j+1] = tmp;
-        }
-    }
-
-    public static void shellSort(int[] array) {
-        int[] drr = new int[]{5, 3, 1};
-        for (int i = 0; i < drr.length; i++) {
-            shell(array, drr[i]);
-        }
-
-    }
-
-    private static void shell(int[] array, int gap) {
-        int j = 0;
-        for (int i = gap; i < array.length; i++) {
-            int tmp = array[i];
-            for (j = i - gap; j >= 0; j -= gap) {
-                if (array[j] > tmp) {
-                    array[j+gap] = array[j];
-                } else {
-                    break;
-                }
-            }
-            array[j+gap] = tmp;
-        }
-    }
-
-    public static void main(String[] args) {
-        int[] array = new int[]{1, 5, 6, 3, 2, 8, 9, 0, 10, 4, 7};
-        shellSort(array);
-        System.out.println(Arrays.toString(array));
-    }
-
-
-
+#include <ioCC2530.h>
+#define uint unsigned int
+#define uchar unsigned char
+#define RLED P1_0
+#define YLED P1_1
+void Delay(uint):
+void Initial(void);
+void Delay(uint n)//延时函数
+{
+   uint tt.jj
+   for(tt=0;tt<n;tt++)
+    for(jj=O;jj<ll0;jj+);
 }
-
+void Initial(void)//初始化
+{
+   PIDIR |= 0x03: //p1.0,p1.1为输出
+   P1IEN |= 0x0C; //开引脚中断
+   PICTL |= 0X02; //开端口使能
+   IEN2 |= 0X10; //P口使能
+   EА=1; //总中断;
+}
+#pragma vector = P1INT_VECTOR
+__interrupt void P1_INTERRUPT(void) { //中断函数
+	if (P1IFG == 0x04 )
+	{
+		RLED = ! RLED;
+		Delay(2000);
+	}
+	if (P1IFG == 0x08) 
+	{
+		GLED = ! GLED;
+		Delay(2000);
+	}
+	P1IFG = O; //P1引脚中断标志位清0
+	P1IF = 0; //P1口中断标志位清零
+}
+void main(void)
+{
+	Initial();
+	while(1) {
+		P1_INTERRUPT();
+	}
+}
--- java_test/TestDemo2/src/com/test/JavaTimerDemo.java
+++ java_test/TestDemo2/src/com/test/JavaTimerDemo.java
@@ -23,4 +23,5 @@ public class JavaTimerDemo {
             TimeUnit.SECONDS.sleep(1);
         }
     }
+
 }
