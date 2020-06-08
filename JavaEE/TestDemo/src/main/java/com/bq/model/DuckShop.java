f56364035667114cd00bcc944e795b9076c28333
123 <897131608@qq.com>
2020-06-08 20:42:26 +0800

    学习spring代码

---
+++ JavaEE/TestDemo/src/main/java/com/bq/model/DuckShop.java
@@ -0,0 +1,25 @@
+package com.bq.model;
+
+import java.util.List;
+
+public class DuckShop {
+
+    private List<Duck> ducks;
+
+    public List<Duck> getDucks() {
+        return ducks;
+    }
+
+    public void setDucks(List<Duck> ducks) {
+        this.ducks = ducks;
+    }
+
+    @Override
+    public String toString() {
+        return "DuckShop{" +
+                "ducks=" + ducks +
+                '}';
+    }
+
+
+}
