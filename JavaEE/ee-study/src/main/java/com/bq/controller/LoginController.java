0076abecafcf2434c42c7592c6ad896217622b53
123 <897131608@qq.com>
2020-06-10 22:37:26 +0800

    提交测试代码

--- JavaEE/ee-study/src/main/java/com/bq/controller/LoginController.java
+++ JavaEE/ee-study/src/main/java/com/bq/controller/LoginController.java
@@ -1,11 +1,12 @@
 package com.bq.controller;
 
 import model.User;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 import java.util.Date;
 
 public class LoginController {
@@ -25,5 +26,22 @@ public class LoginController {
         user.setBirthday(new Date());
         return user;
     }
+
+    @RequestMapping(value = "/login5", method = RequestMethod.POST)
+    @ResponseBody //解析为json格式
+    public Object login3(HttpServletRequest request, HttpServletResponse response,
+                         @RequestBody User u) {
+        if ("abc".equals(u.getUsername()) && "123".equals(u.getPassword())) {
+            HttpSession session = request.getSession();
+            session.setAttribute("user",u);
+        }
+        User user = new User();
+        user.setUsername("bq");
+        user.setPassword("123");
+        user.setBirthday(new Date());
+        return user;
+
+    }
+
 
 }
