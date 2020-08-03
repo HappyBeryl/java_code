95d396aa78e40e9044fc543ed974745dba7cf2a0
123 <897131608@qq.com>
2020-08-03 21:18:15 +0800

    提交测试代码

---
+++ java_test/TestDemo2/src/com/test/Entity.java
@@ -0,0 +1,17 @@
+package com.test;
+
+public class Entity {
+
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+
+
+}
