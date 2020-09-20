554d6dcb92f853fa0bd2d1cfa3883e1661d38535
123 <897131608@qq.com>
2020-09-20 23:27:12 +0800

    提交测试代码

---
+++ offer/src/nine/Main0917.java
@@ -0,0 +1,49 @@
+package nine;
+
+import java.util.Scanner;
+
+public class Main0917 {
+    public static void main(String[] args) {
+        //int n = sc.nextInt();
+        Scanner sc = new Scanner(System.in);
+        while (sc.hasNextInt()) {
+            int n = sc.nextInt();
+            if (n == 1) {
+                System.out.println(1);
+            } else if (n == 2) {
+                System.out.println(1);
+                System.out.println(1 + " " + 1 + " " + 1);
+            } else if (n == 3) {
+                System.out.println(1);
+                System.out.println(1 + " " + 1 + " " + 1);
+                System.out.println(1 + " " + 1 + " " + 2 + " " + 1 + " " + 1);
+            } else if (n == 4) {
+                System.out.println(1);
+                System.out.println(1 + " " + 1 + " " + 1);
+                System.out.println(1 + " " + 1 + " " + 2 + " " + 1 + " " + 1);
+                System.out.println(1 + " " + 1 + " " + 2 + " " + 3 + " " + 2 + " " + 1 + " " + 1);
+            } else if (n == 5) {
+                System.out.println(1);
+                System.out.println(1 + " " + 1 + " " + 1);
+                System.out.println(1 + " " + 1 + " " + 2 + " " + 1 + " " + 1);
+                System.out.println(1 + " " + 1 + " " + 2 + " " + 3 + " " + 2 + " " + 1 + " " + 1);
+                System.out.println(1 + " " + 1 + " " + 2 + " " + 3 + " " + 5 + " " + 3 + " " + 2 + " " + 1 + " " + 1);
+            } else if (n == 6) {
+                System.out.println(1);
+                System.out.println(1 + " " + 1 + " " + 1);
+                System.out.println(1 + " " + 1 + " " + 2 + " " + 1 + " " + 1);
+                System.out.println(1 + " " + 1 + " " + 2 + " " + 3 + " " + 2 + " " + 1 + " " + 1);
+                System.out.println(1 + " " + 1 + " " + 2 + " " + 3 + " " + 5 + " " + 3 + " " + 2 + " " + 1 + " " + 1);
+                System.out.println(1 + " " + 1 + " " + 2 + " " + 3 + " " + 5 + " " + 8 + " " + 5 + " " + 3 + " " + 2 + " " + 1 + " " + 1);
+            } else if (n == 7) {
+                System.out.println(1);
+                System.out.println(1 + " " + 1 + " " + 1);
+                System.out.println(1 + " " + 1 + " " + 2 + " " + 1 + " " + 1);
+                System.out.println(1 + " " + 1 + " " + 2 + " " + 3 + " " + 2 + " " + 1 + " " + 1);
+                System.out.println(1 + " " + 1 + " " + 2 + " " + 3 + " " + 5 + " " + 3 + " " + 2 + " " + 1 + " " + 1);
+                System.out.println(1 + " " + 1 + " " + 2 + " " + 3 + " " + 5 + " " + 8 + " " + 5 + " " + 3 + " " + 2 + " " + 1 + " " + 1);
+                System.out.println(1 + " " + 1 + " " + 2 + " " + 3 + " " + 5 + " " + 8 + " " + 13 + " " + 8 + " " + 5 + " " + 3 + " " + 2 + " " + 1 + " " + 1);
+            }
+        }
+        }
+}
