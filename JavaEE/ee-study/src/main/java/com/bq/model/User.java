8d3361a20ee70e85ab1a4b4f8aed3975f52449c6
123 <897131608@qq.com>
2020-10-11 18:25:23 +0800

    提交测试代码

--- JavaEE/ee-study/src/main/java/com/bq/model/User.java
+++ JavaEE/ee-study/src/main/java/com/bq/model/User.java
@@ -1,18 +1,44 @@
-package frank.model;
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <strings.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
 
-import lombok.Getter;
-import lombok.Setter;
-import lombok.ToString;
+int main()
+{
+	int ret;
+	int fd = open("a.txt", O_RDONLY);
+	if (fd < 0) 
+	{
+		perror("open");
+		return -1;
+	}
+	char buf[101];
+	memset(buf, 0, sizeof(buf)); 
+	ret = read(fd, buf, sizeof(buf) - 1);
+	if (ret < 0)
+	{
+		perror("read");
+		return -1;
+	}
+	close(fd);
 
-import java.util.Date;
+	fd = 0;
+	fd = open("b.txt", O_WRONLY | O_CREAT | O_TRUNC, 0666);
+	if (fd < 0)
+	{
+		perror("open");
+		return -1;
+	}
+	if (0 > write(fd, buf, strlen(buf))) 
+	{
+		perror("write");
+		return -1;
+	}
+	close(fd);
 
-@Getter
-@Setter
-@ToString
-public class User {
-
-    private String username;
-    private String password;
-    private Date birthday;
-
+	return 0;
 }
---
+++ offer/src/nine/Main1011.java
@@ -0,0 +1,50 @@
+package nine;
+
+import java.util.Scanner;
+
+public class Main1011 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String str = sc.nextLine();
+        String[] arrString = str.split(",");
+        int[] arr = new int[arrString.length];
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = Integer.parseInt(arrString[i]);
+        }
+        int min = arr[0];
+        int max = arr[1]-min;
+        int i = 2;
+        while (i < arr.length) {
+            if (arr[i-1] < min) {
+                min = arr[i-1];
+            }
+            int tmp = arr[i]-min;
+            if (tmp > max) {
+                max = tmp;
+            }
+            i++;
+        }
+        if (max < 0) {
+            System.out.println(0);
+        } else {
+            System.out.println(max);
+        }
+    }
+
+    public static void main1(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int a = 1;
+        int b = 2;
+        int c = n;
+        while (n > 2) {
+            c = a+b;
+            a = b;
+            b = c;
+            n--;
+        }
+        System.out.println(c);
+    }
+
+
+}
