57ff4d0dc04045d6d0c25b384519ad993b179f44
123 <897131608@qq.com>
2020-10-22 18:11:06 +0800

    二叉树and字符串转化为整数

--- JavaEE/ee-study/src/main/java/com/bq/model/User.java
+++ JavaEE/ee-study/src/main/java/com/bq/model/User.java
@@ -1,44 +1,76 @@
-#include <stdio.h>
-#include <string.h>
-#include <stdlib.h>
-#include <strings.h>
-#include <sys/types.h>
-#include <sys/stat.h>
-#include <fcntl.h>
-#include <unistd.h>
+ 16:49:48
 
-int main()
+
+//头文件的包含和家定义
+#include <ioCC2530.h>
+
+#defineuint unsigned int
+#define uchar unsigned char
+#define LED2 P1_0
+#derine LED3 P1_1
+#define S1 P1_2
+#defineS3 P1_3
+
+void InitialLed (void); //化初始化灯函数声明
+void InitialKey(void);  //初始化按键函数声明
+
+void Delay(uint n)
 {
-	int ret;
-	int fd = open("a.txt", O_RDONLY);
-	if (fd < 0) 
-	{
-		perror("open");
-		return -1;
-	}
-	char buf[101];
-	memset(buf, 0, sizeof(buf)); 
-	ret = read(fd, buf, sizeof(buf) - 1);
-	if (ret < 0)
-	{
-		perror("read");
-		return -1;
-	}
-	close(fd);
-
-	fd = 0;
-	fd = open("b.txt", O_WRONLY | O_CREAT | O_TRUNC, 0666);
-	if (fd < 0)
-	{
-		perror("open");
-		return -1;
-	}
-	if (0 > write(fd, buf, strlen(buf))) 
-	{
-		perror("write");
-		return -1;
-	}
-	close(fd);
-
-	return 0;
+	uint i,j;
+	for(i=0;i<n;i++)
+	for(j=0;j<110: j++) ;
 }
+void InitialLed (void)
+{
+	P1SEL &= 0x03;
+	P1DIR |= 0x03; //将P10、P11定义为输出
+	LED2=1:
+	LED3=1;
+}
+
+void InitialKey(void)
+{
+	EA=1;//开全局中断
+	IEN2=0x10; //P1中断使能
+	PIIEN =0x0c; //开引脚中断
+	PICTL=0x02; //开端口中断
+}
+
+//中断处理函数
+
+#pragma vector =P1INT VECTOR 
+interrupt void P1_ ISR(void)
+{
+    uint i
+	if(P1IFG==0x04) //判断按键S1是否按下
+	{
+ 		for(i=0;i<20;i++)//LED2闪烁10次
+		{
+			LED2= !LED2;
+			Delay(1000);
+		}
+	if(P1IFG=0x08)//判断按键S2是否按下
+	{
+		for(i=0; i<20;i++)//LED3闪烁10次\
+		{
+			LED3=!LED3;
+			Delay(1000) ;
+		}
+	}
+	P1IFG=0;
+	P1IF=0;//清中断标志
+	LED2=1;
+	LED3=1;
+//主函数
+void main(void)
+{
+	InitialLed();
+	InitialKey();
+	while(1) 
+	{
+		PCON|=0x01;
+		SLEEPCMD|=0x03;
+	}
+}
+
+
---
+++ java_test/TestDemo2/src/test/TreeNode.java
@@ -0,0 +1,65 @@
+package test;
+
+public class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+    TreeNode(int x) { val = x; }
+
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        if (root == null) {
+            return null;
+        }
+        if (p == root || q == root) {
+            return root;
+        }
+        TreeNode leftNode = lowestCommonAncestor(root.left, p, q);
+        TreeNode rightNode = lowestCommonAncestor(root.right, p, q);
+        if (leftNode != null && rightNode != null) {
+            return root;
+        }
+        if (leftNode != null) {
+            return leftNode;
+        }
+        if (rightNode != null) {
+            return rightNode;
+        }
+        return null;
+    }
+
+    public TreeNode lowestCommonAncestor1(TreeNode root, TreeNode p, TreeNode q) {
+        if (root == null) {
+            return null;
+        }
+        while (root != null) {
+            if (root.val < p.val && root.val < q.val) {
+                root = root.right;
+            } else if (root.val > p.val && root.val > q.val) {
+                root = root.left;
+            } else {
+                break;
+            }
+        }
+        return root;
+    }
+
+    public int strToInt(String str) {
+        char[] arr = str.trim().toCharArray();
+        if(arr.length == 0) return 0;
+        int sign = 1;
+        int i = 1;
+        int res = 0;
+        int maxmin = Integer.MAX_VALUE/10;
+        if (arr[0] == '-') sign = -1;
+        else if (arr[0] != '+') i = 0;
+        for (int j = i; j < arr.length; j++) {
+            if (arr[j] < '0' || arr[j] > '9') break;
+            if(res > maxmin || res == maxmin && arr[j] > '7') {
+                return sign == 1 ? Integer.MAX_VALUE : Integer.MIN_VALUE;
+            }
+            res = res * 10 + (arr[j] - '0');
+        }
+        return sign*res;
+    }
+    
+}
